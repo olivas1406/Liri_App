af38004a50888a9cd2b6444d422a4c25e7aa7c55
olivas1406 <olivas1406@msn.com>
2018-04-04 02:12:04 -0700

    2nd input can now take multiple words for songs/movies

--- liri.js
+++ liri.js
@@ -15,20 +15,29 @@ var keys = require("./keys.js")
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
-var inputString = process.argv.slice(2);
+var args = process.argv.slice(2);
 
-var command = inputString[0];   // what the command is
+var toDo = args[0];
 
-var toDo = inputString[1];      // the command argument (song name, movie name, etc...)
+var toDoArray = [];
 
-if (command === "my-tweets") {  // node liri.js my-tweets
+for (let i = 1; i < args.length; i++) {
+    toDoArray.push(args[i]);
+}
+
+
+console.log("Command Given: " + toDo)
+console.log(toDoArray);
+
+
+if (toDo === "my-tweets") {  // node liri.js my-tweets
     // This will show your last 20 tweets and when they were created at in your terminal/bash window
-} else if (command === "spotify-this-song") { // node liri.js spotify-this-song '<song name here>'
+} else if (toDo === "spotify-this-song") { // node liri.js spotify-this-song '<song name here>'
     // This will show the following information about the song in your terminal/bash window
     // artist, the song's name, a preview link of the song from spotify, the album the song is from
     // If no song is provided then your program will default to "The Sign" by Ace of Base.
     // use node-spotify API for this
-} else if (command === "movie-this") { // node liri.js movie-this '<movie name here>'
+} else if (toDo === "movie-this") { // node liri.js movie-this '<movie name here>'
     /*
     * Title of the movie.
     * Year the movie came out.
@@ -41,14 +50,18 @@ if (command === "my-tweets") {  // node liri.js my-tweets
     */
     // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
     // You'll use the request package to retrieve data from the OMDB API. Like all of the in-class activities, 
-    // the OMDB API requires an API key. You may use trilogy.
-} else if (command === "do-what-it-says") {  // node liri.js do-what-it-says
-    /*
-    Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
-    It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
-    Feel free to change the text in that document to test out the feature for other commands
-    */
-};
+    // the OMDB API requires an API key. You may use trilogy.                    
+
+    } else if (toDo === "do-what-it-says") {  // node liri.js do-what-it-says
+    
+   // Using the fs Node package, LIRI will take the text inside of random.txt and then use it to call one of LIRI's commands.
+   // It should run spotify-this-song for "I Want it That Way," as follows the text in random.txt.
+   // Feel free to change the text in that document to test out the feature for other commands
+    
+
+ } else {
+    console.log("Please Enter A Valid Input")
+}
 
 
 
