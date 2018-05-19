721a9d4c77c5806d0fc57315258b2d1295a15b81
Daniel Olivas <olivas1406@msn.com>
2018-05-18 22:15:44 -0700

    Cleaned up code

--- keys.js
+++ keys.js
@@ -1,7 +1,4 @@
 
-
-// console.log('this is loaded');
-
 exports.twitter = {
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -13,9 +10,3 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
-
-
-
-
-
-
